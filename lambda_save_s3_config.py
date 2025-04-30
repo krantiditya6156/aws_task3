0d416e4e2975e7d1267306e7bb3f98b40a441f50
Krantiditya .(krantiditya) <krantiditya@infosys.com>
2025-04-30 14:14:31 +0530

    initial commit

---
+++ lambda_save_s3_config.py
@@ -0,0 +1,36 @@
+import json
+import boto3
+
+s3_client = boto3.client('s3')
+dynamodb_client = boto3.resource('dynamodb')
+TABLE_NAME = 's3file-config-table'
+
+def lambda_handler(event, context):
+    print(event)
+
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_name = event['Records'][0]['s3']['object']['key']
+
+    response = s3_client.head_object(Bucket=bucket_name, Key=file_name)
+
+    tags = s3_client.get_object_tagging(Bucket=bucket_name, Key=file_name)
+
+
+    table = dynamodb_client.Table(TABLE_NAME)
+    table.put_item(
+        Item={
+            'object_key': file_name,
+            'version_id': response['VersionId'],
+            'metadata': response['Metadata'],
+            'tags': tags['TagSet'],
+            'file_size': response['ContentLength'],
+            'content_type': response['ContentType'],
+            'last_modified_data': response['LastModified'].strftime("%m/%d/%Y, %H:%M:%S")
+        }
+    )
+
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('saved into dynamodb table')
+    }
