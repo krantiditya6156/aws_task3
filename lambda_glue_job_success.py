9b754e54ae062f18ee92cc6c0c8ad68ec01f2a2b
Krantiditya .(krantiditya) <krantiditya@infosys.com>
2025-05-09 14:29:45 +0530

    lambda glue job success

--- lambda_glue_job_success.py
+++ lambda_glue_job_success.py
@@ -52,8 +52,9 @@ def get_table_name(crawler_name):
     try:
         client = boto3.client("glue")
         response = client.get_crawler(Name=crawler_name)
+        print(response)
         table_name = response["Crawler"]["Targets"]["S3Targets"][0]["Path"].split("/")[
-            -1
+            -2
         ]
         return table_name
     except Exception as e:
@@ -63,13 +64,14 @@ def get_table_name(crawler_name):
 
 def run_athena_query(query, database):
     try:
-        client = boto3.client("athena")
+        client = boto3.client("athena", region_name="ap-south-1")
         response = client.start_query_execution(
             QueryString=query,
             QueryExecutionContext={"Database": database},
             ResultConfiguration={"OutputLocation": ATHENA_OUTPUT},
         )
         print(response)
+        return response
     except Exception as e:
         print(e)
         raise e
@@ -89,12 +91,24 @@ def lambda_handler(event, context):
 
             if get_crawler_state(crawler_name) == "READY":
                 run_crawler(crawler_name)
+            else:
+                raise Exception("Crawler is not ready.")
 
-            # table_name = get_table_name(crawler_name)
+            table_name = get_table_name(crawler_name)
+            query = f"SELECT * FROM {table_name} LIMIT 10;"
+            print(f"Query: {query}")
 
-            # print(table_name)
-            # query = f"SELECT * FROM {table_name} LIMIT 10;"
-            # run_athena_query(QUERY, DATABASE)
+            response = run_athena_query(query, DATABASE)
+
+            if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+                query_execution_id = response["QueryExecutionId"]
+                print(f"Query execution id is {query_execution_id}")
+                print(
+                    f"Query executed successfully and result is saved in s3 bucket {ATHENA_OUTPUT} "
+                )
+            else:
+                print("Query execution failed.")
+                raise Exception("Query execution failed.")
 
     except Exception as e:
         print(e)
